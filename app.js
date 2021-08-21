653e4e2442b76952c50b9a205d5ecb574d6e4f28
Jason Thien <40978303+jsx101@users.noreply.github.com>
2021-08-22 02:17:23 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res)=> {
+    res.send("hello");
+});
+
+app.get("/hello/:name", (req,res)=>{
+    if(req.params.name === "Janet") {
+        res.send("Go away, Janet!");
+    }
+    res.send(`Hello, ${req.params.name}`);
+});
+
+app.listen(80, () => {
+    console.log("App listening on port 80!");
+  });
